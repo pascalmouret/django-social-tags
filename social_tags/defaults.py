a29b77fe17d02ffb2b2038d97c928a050ba14ab2
Pascal Mouret <pascal.mouret@divio.ch>
2013-01-19 19:10:59 +0100

    experimental twitter support

--- social_tags/defaults.py
+++ social_tags/defaults.py
@@ -3,7 +3,9 @@ from social_tags import settings
 
 
 DEFAULT_SETTINGS = {
+    'card': 'summary',
     'type': settings.DEFAULT_TYPE,
     'image': settings.DEFAULT_IMAGE,
     'locales': settings.DEFAULT_LOCALES,
+    'description': settings.DEFAULT_DESCRIPTION,
 }
--- social_tags/networks.py
+++ social_tags/networks.py
@@ -1,9 +1,12 @@
 # -*- coding: utf-8 -*-
+from django.core.files.images import ImageFile
+
 from social_tags.objects import RenderObject
 
 
 AVAILABLE = {
     'opengraph': 'OpenGraph',
+    'twitter': 'Twitter',
 }
 
 class OpenGraph(RenderObject):
@@ -14,3 +17,32 @@ class OpenGraph(RenderObject):
             context[key] = value
         context['locales'] = [l for l in context['locales'] if l != context['locale']]
         return context
+
+
+class Twitter(RenderObject):
+    template = 'social_tags/networks/twitter.html'
+
+    def prepare_context(self, context):
+        if context['card'] == 'summary':
+            return self.get_summary_context(context)
+        if context['card'] == 'photo':
+            return self.get_photo_context(context)
+        if context['card'] == 'player':
+            return self.get_player_context(context)
+
+    def get_summary_context(self, context):
+        return context
+
+    def get_photo_context(self, context):
+        try:
+            pic = ImageFile(open(context['image']), 'r')
+        except IOError:
+            return context
+        if not hasattr(context, 'twitter'):
+            context['twitter'] = {}
+        context['twitter']['width'] = pic.width
+        context['twitter']['height'] = pic.height
+        return context
+
+    def get_player_context(self, context):
+        raise NotImplementedError('Not yet implemented.')
