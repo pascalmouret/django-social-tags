eb07a52e2f4f9d430f27f1777aa0613331bf7250
Pascal Mouret <pascal.mouret@divio.ch>
2013-01-20 16:05:17 +0100

    SOME cleanup in the twitter stuff

--- social_tags/defaults.py
+++ social_tags/defaults.py
@@ -3,12 +3,12 @@ from social_tags import settings
 
 
 DEFAULT_SETTINGS = {
-    'card': 'summary',
     'type': settings.DEFAULT_TYPE,
     'image': settings.DEFAULT_IMAGE,
     'locales': settings.DEFAULT_LOCALES,
     'description': settings.DEFAULT_DESCRIPTION,
     'twitter': {
+        'card': 'summary',
         'site_id': settings.DEFAULT_TWITTER_SITE_ID,
         'creator_id': settings.DEFAULT_TWITTER_CREATOR_ID,
     }
--- social_tags/networks.py
+++ social_tags/networks.py
@@ -4,10 +4,15 @@ from django.core.files.images import ImageFile
 from social_tags.objects import RenderObject
 
 
+class SocialTagsValidationError(Exception):
+    pass
+
+
 AVAILABLE = {
     'opengraph': 'OpenGraph',
     'twitter': 'Twitter',
 }
+
 
 class OpenGraph(RenderObject):
     template = 'social_tags/networks/opengraph.html'
@@ -23,11 +28,11 @@ class Twitter(RenderObject):
     template = 'social_tags/networks/twitter.html'
 
     def prepare_context(self, context):
-        if context['card'] == 'summary':
+        if context['twitter']['card'] == 'summary':
             return self.get_summary_context(context)
-        if context['card'] == 'photo':
+        if context['twitter']['card'] == 'photo':
             return self.get_photo_context(context)
-        if context['card'] == 'player':
+        if context['twitter']['card'] == 'player':
             return self.get_player_context(context)
 
     def get_summary_context(self, context):
@@ -46,14 +51,14 @@ class Twitter(RenderObject):
         return context
 
     def debug(self, context):
-        if context['card'] == 'summary':
+        if context['twitter']['card'] == 'summary':
             if not context['description']:
-                raise Exception('Description is required for summary card.')
-        if context['card'] == 'photo':
+                raise SocialTagsValidationError('Description is required for summary card.')
+        if context['twitter']['card'] == 'photo':
             if not context['image']:
-                raise Exception('Image is required for photo card')
-        if context['card'] == 'player':
+                raise SocialTagsValidationError('Image is required for photo card')
+        if context['twitter']['card'] == 'player':
             if not context['twitter']['player'] or not context['twitter']['width'] or not context['twitter']['height']:
-                raise Exception('Player, width and height are required for player card')
+                raise SocialTagsValidationError('Player, width and height are required for player card')
             if context['twitter']['stream'] and not context['twitter']['content_type']:
-                raise Exception('If stream is set, content_type is required.')
+                raise SocialTagsValidationError('If stream is set, content_type is required.')
