88c3abcc02ac608809cb0fde2ff609dd2e132fc9
Pascal Mouret <pascal.mouret@divio.ch>
2013-01-29 16:52:01 +0100

    opengraph debug

--- social_tags/social_tags_networks/opengraph.py
+++ social_tags/social_tags_networks/opengraph.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from social_tags.networks import Network, networks
+from social_tags.networks import Network, networks, SocialTagsValidationError
 
 
 class OpenGraph(Network):
@@ -11,4 +11,12 @@ class OpenGraph(Network):
             context[key] = value
         context['locales'] = [l for l in context['locales'] if l != context['locale']]
         return context
+
+    def debug(self, context):
+        if not hasattr(context, 'url'):
+            raise SocialTagsValidationError('The url settings in required.')
+        if not hasattr(context, 'title'):
+            raise SocialTagsValidationError('The title setting is required.')
+        if not hasattr(context, 'locale'):
+            raise SocialTagsValidationError('The locale setting in required.')
 networks.register(OpenGraph)
